abe80d12a1016592c3f73a52ca14008787052460
fidelp27 <fidel2702@gmail.com>
2023-02-02 06:49:21 -0300

    intersection

---
+++ intersection.py
@@ -0,0 +1,27 @@
+'''
+Input Format
+
+The first line contains , the number of students who have subscribed to the English newspaper.
+The second line contains  space separated roll numbers of those students.
+The third line contains , the number of students who have subscribed to the French newspaper.
+The fourth line contains  space separated roll numbers of those students.
+
+Output Format
+
+Output the total number of students who have subscriptions to both English and French newspapers.
+
+Sample Input
+
+9
+1 2 3 4 5 6 7 8 9
+9
+10 1 2 3 11 21 55 6 8
+Sample Output
+
+5
+'''
+e, *m = map(int, input().split())
+f, *n = map(int, input().split())
+
+inter = set(m) & (set(n))
+print(len(inter))
