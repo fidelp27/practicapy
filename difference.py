939fa11747a0fa652e548fe2a2c856909766fb38
fidelp27 <fidel2702@gmail.com>
2023-02-07 06:30:44 -0300

    difference -> conjuntos

---
+++ difference.py
@@ -0,0 +1,22 @@
+'''
+.difference()
+The tool .difference() returns a set with all the elements from the set that are not in an iterable.
+
+Task
+Students of District College have a subscription to English and French newspapers. Some students have subscribed to only the English newspaper, some have subscribed to only the French newspaper, and some have subscribed to both newspapers.
+
+You are given two sets of student roll numbers. One set has subscribed to the English newspaper, and one set has subscribed to the French newspaper. Your task is to find the total number of students who have subscribed to only English newspapers.
+
+Input Format
+
+The first line contains the number of students who have subscribed to the English newspaper.
+The second line contains the space separated list of student roll numbers who have subscribed to the English newspaper.
+The third line contains the number of students who have subscribed to the French newspaper.
+The fourth line contains the space separated list of student roll numbers who have subscribed to the French newspaper.
+'''
+
+n, e = int(input()), input().split()
+m, f = int(input()), input().split()
+
+print(set(e).difference(set(f)))
+print(len(set(e).difference(set(f))))
