3af17978b6bc2a0f025a1b81983c4fbbd7a467bf
fidelp27 <fidel2702@gmail.com>
2023-03-08 06:18:49 -0300

    fix regex_search

--- regex_search.py
+++ regex_search.py
@@ -5,5 +5,8 @@ of an alphanumeric character in S (read from left to right) that has consecutive
 '''
 
 import re
-m = re.search(r'(.)\1+', input())
-print(m.group(1))
+m = re.search(r'([a-zA-Z0-9])\1+', input())
+if (m):
+    print(m.group(1))
+else:
+    print(-1)
